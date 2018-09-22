3cc1e7273ec5e83f8e19399350ea1a1382f25171
ShadowHaund <edvard.dunijsk@mail.ru>
2018-09-22 22:03:29 +0300

    Первые два урока

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -57,9 +57,7 @@ fun daysInMonth(month: Int, year: Int): Int {
  */
 
 fun circleInside(x1: Double, y1: Double, r1: Double,
-                 x2: Double, y2: Double, r2: Double): Boolean {
-    return sqr(x2 - x1) * sqr(y2 - y1) <= (r2 - r1)
-}
+                 x2: Double, y2: Double, r2: Double): Boolean = sqr(x2 - x1) * sqr(y2 - y1) <= (r2 - r1)
 /**
  * Средняя
  *
