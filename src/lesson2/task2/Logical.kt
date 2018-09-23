c2629009e18022701ff7aea7c3b9b426ede6f7b3
ShadowHaund <edvard.dunijsk@mail.ru>
2018-09-23 14:42:01 +0300

    Первые два урока

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -3,6 +3,8 @@
 package lesson2.task2
 
 import lesson1.task1.sqr
+import kotlin.math.abs
+import kotlin.math.sqrt
 
 /**
  * Пример
@@ -57,7 +59,8 @@ fun daysInMonth(month: Int, year: Int): Int {
  */
 
 fun circleInside(x1: Double, y1: Double, r1: Double,
-                 x2: Double, y2: Double, r2: Double): Boolean = sqr(x2 - x1) * sqr(y2 - y1) <= (r2 - r1)
+                 x2: Double, y2: Double, r2: Double): Boolean =
+        abs(r2-r1) <= sqrt(sqr(x2 - x1) + sqr(y2 - y1)) && sqrt(sqr(x2 - x1) + sqr(y2 - y1)) <= (r2 - r1)
 /**
  * Средняя
  *
